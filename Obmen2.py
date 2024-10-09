cc0c84576d7efc2d0d143a880d0e43c37187b389
irina-Sk <skivfarag@gmail.com>
2024-10-09 21:07:37 +0300

    функционал обменника

--- Obmen2.py
+++ Obmen2.py
@@ -1,7 +1,29 @@
+from http.client import responses
+
 import requests
 import json
 from tkinter import*
 from tkinter import messagebox as mb
+
+
+def exchange():
+    code = entry.get()
+    if code:
+        try:
+            response = requests.get("https://open.er-api.com/v6/latest/USD")
+            response.raise_for_status()
+            data = response.json()
+            if code in data['rates']
+                exchange_rate = ['rates'][code]
+                mb.showinfo("курс обмена", f"Курс {exchange_rate}-{code} за доллар")
+            else:
+                mb.showerror("Ошибка", f"валюта {code} не найдена")
+
+        except Exceotion as e:
+            mb.showerror("Ошибка", f"ошибка {e}")
+    else:
+        mb.showwarning("Внимание!"б f"Введите код валюты")
+
 
 window = Tk()
 window.title("Курс обмена валют")
